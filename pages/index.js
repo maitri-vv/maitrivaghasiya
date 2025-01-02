d61f79eeaa19125b63d663f457dd93d0bb8e9f5f
maitri-vv <maitrivaghasiya99@gmail.com>
2025-01-02 22:17:49 +0530

    vs code changes

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Index({ posts, globalData }) {
   return (
     <Layout>
       <SEO title={globalData.name} description={globalData.blogTitle} />
-      <Header name={globalData.name} />
+      <Header name="Maitri Vaghasiya" />
       <main className="w-full">
         <h1 className="mb-12 text-3xl text-center lg:text-5xl">
           {globalData.blogTitle}
